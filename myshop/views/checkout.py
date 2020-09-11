4cb6b365498ab04bca0348a2c2b26299643cd64a
wayoftree <jain.2222.j@gmail.com>
2020-09-11 20:51:03 +0530

    checkout fixed

--- myshop/views/checkout.py
+++ myshop/views/checkout.py
@@ -7,6 +7,10 @@ from myshop.models import Customer
 
 
 class CheckOut(View):
+
+    def get(self, request):
+        return redirect('cart')
+
     def post(self, request):
         address = request.POST.get('address')
         phone = request.POST.get('phone')
@@ -14,16 +18,20 @@ class CheckOut(View):
         cart = request.session.get('cart')
         products = Product.get_products_by_id(list(cart.keys()))
         print(address,phone,customer,cart,products)
+        
+        if customer:
+            for product in products:
+                order = Order(customer = Customer(id=customer),
+                            product = product,
+                            price = product.price,
+                            address = address,
+                            phone = phone,
+                            quantity = cart.get(str(product.id)))
+                order.save()
+                request.session['cart'] = {}
+        else:
+            returnUrl = request.META['PATH_INFO']
+            print(returnUrl)
+            return redirect(f'/customer_login?next={returnUrl}')
 
-        for product in products:
-            print(cart.get(str(product.id)))
-            order = Order(customer=Customer(id=customer),
-                          product=product,
-                          price=product.price,
-                          address=address,
-                          phone=phone,
-                          quantity=cart.get(str(product.id)))
-            order.save()
-        request.session['cart'] = {}
-
-        return redirect('index')
+        return redirect('index') 
